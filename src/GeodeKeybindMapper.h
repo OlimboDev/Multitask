47148c466b9fad4fd140a6c05873ba64e73a7189
Robin <robinrid6@gmail.com>
2026-02-27 02:42:23 +0100

    Refactor GeodeKeybindMapper and main module for improved key mapping and event handling

--- src/GeodeKeybindMapper.h
+++ src/GeodeKeybindMapper.h
@@ -1,223 +1,134 @@
 #pragma once
 
 #include <Geode/Geode.hpp>
-#include <Geode/loader/Mod.hpp>
+#include <Geode/loader/SettingV3.hpp>
+#include <Geode/loader/GameEvent.hpp>
 #include <optional>
-#include <unordered_map>
 
 using namespace geode::prelude;
 
-// ============================================================
-//  GeodeKeybindMapper
-//
-//  Maps between cocos2d::enumKeyCodes (used by geode::Keybind)
-//  and Windows Virtual-Key codes (winuser.h VK_* constants).
-//
-//  Usage
-//  -----
-//  // Get VK code from a setting
-//  auto binds = Mod::get()->getSettingValue<std::vector<geode::Keybind>>("firstPlayerUpKey");
-//  if (!binds.empty()) {
-//      auto vk = GeodeKeybindMapper::geodeKeyToVK(binds[0].key);
-//      if (vk) { /* use *vk */ }
-//  }
-//
-//  // Reverse-lookup
-//  auto key = GeodeKeybindMapper::vkToGeodeKey(VK_SPACE);
-// ============================================================
-
-class GeodeKeybindMapper {
+class GeodeKeybindMapper final {
 public:
-    // -------------------------------------------------------
-    // Core lookup tables
-    // -------------------------------------------------------
-
     struct KeyPair {
-        enumKeyCodes  geodeKey;
-        int           vkCode;     // Windows VK_* value
+        enumKeyCodes geodeKey;
+        int vkCode;
     };
 
-    // Full mapping table – every cocos2d key that has a
-    // reasonable Windows VK equivalent.
-    static inline const KeyPair kKeyTable[] = {
-        // --- Special / control keys ---
-        { KEY_Backspace,        VK_BACK        },
-        { KEY_Tab,              VK_TAB         },
-        { KEY_Enter,            VK_RETURN      },
-        { KEY_CapsLock,         VK_CAPITAL     },
-        { KEY_Escape,           VK_ESCAPE      },
-        { KEY_Space,            VK_SPACE       },
-        { KEY_Insert,           VK_INSERT      },
-        { KEY_Home,             VK_HOME        },
-        { KEY_PageUp,           VK_PRIOR       },
-        { KEY_Delete,           VK_DELETE      },
-        { KEY_End,              VK_END         },
-        { KEY_PageDown,         VK_NEXT        },
+    // Every cocos2d key that has a reasonable Windows VK equivalent.
+    static inline const KeyPair s_keyTable[] = {
+        {KEY_Backspace, VK_BACK},
+        {KEY_Tab, VK_TAB},
+        {KEY_Enter, VK_RETURN},
+        {KEY_CapsLock, VK_CAPITAL},
+        {KEY_Escape, VK_ESCAPE},
+        {KEY_Space, VK_SPACE},
+        {KEY_Insert, VK_INSERT},
+        {KEY_Home, VK_HOME},
+        {KEY_PageUp, VK_PRIOR},
+        {KEY_Delete, VK_DELETE},
+        {KEY_End, VK_END},
+        {KEY_PageDown, VK_NEXT},
 
-        // --- Arrow keys ---
-        { KEY_Left,             VK_LEFT        },
-        { KEY_Right,            VK_RIGHT       },
-        { KEY_Up,               VK_UP          },
-        { KEY_Down,             VK_DOWN        },
+        {KEY_Left, VK_LEFT},
+        {KEY_Right, VK_RIGHT},
+        {KEY_Up, VK_UP},
+        {KEY_Down, VK_DOWN},
 
-        // --- Modifier keys ---
-        { KEY_Shift,            VK_SHIFT       },
-        { KEY_Control,          VK_CONTROL     },
-        { KEY_Alt,              VK_MENU        },
+        {KEY_Shift, VK_SHIFT},
+        {KEY_Control, VK_CONTROL},
+        {KEY_Alt, VK_MENU},
 
-        // Note: Geode uses KEY_Shift/Control/Alt for both sides;
-        // if you need left/right distinction use VK_LSHIFT etc.
+        {KEY_Pause, VK_PAUSE},
+        {KEY_ScrollLock, VK_SCROLL},
+        {KEY_PrintScreen, VK_SNAPSHOT},
 
-        // --- Lock / utility ---
-        { KEY_Pause,            VK_PAUSE       },
-        { KEY_ScrollLock,       VK_SCROLL      },
-        { KEY_PrintScreen,      VK_SNAPSHOT    },
+        {KEY_NumPad0, VK_NUMPAD0},
+        {KEY_NumPad1, VK_NUMPAD1},
+        {KEY_NumPad2, VK_NUMPAD2},
+        {KEY_NumPad3, VK_NUMPAD3},
+        {KEY_NumPad4, VK_NUMPAD4},
+        {KEY_NumPad5, VK_NUMPAD5},
+        {KEY_NumPad6, VK_NUMPAD6},
+        {KEY_NumPad7, VK_NUMPAD7},
+        {KEY_NumPad8, VK_NUMPAD8},
+        {KEY_NumPad9, VK_NUMPAD9},
+        {KEY_Multiply, VK_MULTIPLY},
+        {KEY_Add, VK_ADD},
+        {KEY_Subtract, VK_SUBTRACT},
+        {KEY_Decimal, VK_DECIMAL},
+        {KEY_Divide, VK_DIVIDE},
 
-        // --- Numpad ---
-        { KEY_NumPad0,          VK_NUMPAD0     },
-        { KEY_NumPad1,          VK_NUMPAD1     },
-        { KEY_NumPad2,          VK_NUMPAD2     },
-        { KEY_NumPad3,          VK_NUMPAD3     },
-        { KEY_NumPad4,          VK_NUMPAD4     },
-        { KEY_NumPad5,          VK_NUMPAD5     },
-        { KEY_NumPad6,          VK_NUMPAD6     },
-        { KEY_NumPad7,          VK_NUMPAD7     },
-        { KEY_NumPad8,          VK_NUMPAD8     },
-        { KEY_NumPad9,          VK_NUMPAD9     },
-        { KEY_Multiply,         VK_MULTIPLY    },
-        { KEY_Add,              VK_ADD         },
-        { KEY_Subtract,         VK_SUBTRACT    },
-        { KEY_Decimal,          VK_DECIMAL     },
-        { KEY_Divide,           VK_DIVIDE      },
+        {KEY_F1, VK_F1},
+        {KEY_F2, VK_F2},
+        {KEY_F3, VK_F3},
+        {KEY_F4, VK_F4},
+        {KEY_F5, VK_F5},
+        {KEY_F6, VK_F6},
+        {KEY_F7, VK_F7},
+        {KEY_F8, VK_F8},
+        {KEY_F9, VK_F9},
+        {KEY_F10, VK_F10},
+        {KEY_F11, VK_F11},
+        {KEY_F12, VK_F12},
 
-        // --- F-keys ---
-        { KEY_F1,               VK_F1          },
-        { KEY_F2,               VK_F2          },
-        { KEY_F3,               VK_F3          },
-        { KEY_F4,               VK_F4          },
-        { KEY_F5,               VK_F5          },
-        { KEY_F6,               VK_F6          },
-        { KEY_F7,               VK_F7          },
-        { KEY_F8,               VK_F8          },
-        { KEY_F9,               VK_F9          },
-        { KEY_F10,              VK_F10         },
-        { KEY_F11,              VK_F11         },
-        { KEY_F12,              VK_F12         },
+        {KEY_Zero, 0x30},
+        {KEY_One, 0x31},
+        {KEY_Two, 0x32},
+        {KEY_Three, 0x33},
+        {KEY_Four, 0x34},
+        {KEY_Five, 0x35},
+        {KEY_Six, 0x36},
+        {KEY_Seven, 0x37},
+        {KEY_Eight, 0x38},
+        {KEY_Nine, 0x39},
 
-        // --- Digit row (0-9) ---
-        { KEY_Zero,             0x30           },
-        { KEY_One,              0x31           },
-        { KEY_Two,              0x32           },
-        { KEY_Three,            0x33           },
-        { KEY_Four,             0x34           },
-        { KEY_Five,             0x35           },
-        { KEY_Six,              0x36           },
-        { KEY_Seven,            0x37           },
-        { KEY_Eight,            0x38           },
-        { KEY_Nine,             0x39           },
+        {KEY_A, 0x41},
+        {KEY_B, 0x42},
+        {KEY_C, 0x43},
+        {KEY_D, 0x44},
+        {KEY_E, 0x45},
+        {KEY_F, 0x46},
+        {KEY_G, 0x47},
+        {KEY_H, 0x48},
+        {KEY_I, 0x49},
+        {KEY_J, 0x4A},
+        {KEY_K, 0x4B},
+        {KEY_L, 0x4C},
+        {KEY_M, 0x4D},
+        {KEY_N, 0x4E},
+        {KEY_O, 0x4F},
+        {KEY_P, 0x50},
+        {KEY_Q, 0x51},
+        {KEY_R, 0x52},
+        {KEY_S, 0x53},
+        {KEY_T, 0x54},
+        {KEY_U, 0x55},
+        {KEY_V, 0x56},
+        {KEY_W, 0x57},
+        {KEY_X, 0x58},
+        {KEY_Y, 0x59},
+        {KEY_Z, 0x5A},
 
-        // --- Letters (A-Z) ---
-        // Windows VK codes for letters are simply their ASCII
-        // upper-case values (0x41-0x5A).
-        // Geode uses KEY_A .. KEY_Z (lower-case letters on cocos2d).
-        { KEY_A,                0x41           },
-        { KEY_B,                0x42           },
-        { KEY_C,                0x43           },
-        { KEY_D,                0x44           },
-        { KEY_E,                0x45           },
-        { KEY_F,                0x46           },
-        { KEY_G,                0x47           },
-        { KEY_H,                0x48           },
-        { KEY_I,                0x49           },
-        { KEY_J,                0x4A           },
-        { KEY_K,                0x4B           },
-        { KEY_L,                0x4C           },
-        { KEY_M,                0x4D           },
-        { KEY_N,                0x4E           },
-        { KEY_O,                0x4F           },
-        { KEY_P,                0x50           },
-        { KEY_Q,                0x51           },
-        { KEY_R,                0x52           },
-        { KEY_S,                0x53           },
-        { KEY_T,                0x54           },
-        { KEY_U,                0x55           },
-        { KEY_V,                0x56           },
-        { KEY_W,                0x57           },
-        { KEY_X,                0x58           },
-        { KEY_Y,                0x59           },
-        { KEY_Z,                0x5A           },
-
-        // --- OEM / punctuation keys ---
-        // VK_OEM_* values are standard on US layouts.
-        { KEY_Semicolon,        VK_OEM_1       },  // ;  / :
-        { KEY_Slash,            VK_OEM_2       },  // /  / ?
-        { KEY_GraveAccent,      VK_OEM_3       },  // `  / ~
-        { KEY_LeftBracket,      VK_OEM_4       },  // [  / {
-        { KEY_RightBracket,     VK_OEM_6       },  // ]  / }
-        { KEY_Apostrophe,       VK_OEM_7       },  // '  / "
+        {KEY_Semicolon, VK_OEM_1},
+        {KEY_Slash, VK_OEM_2},
+        {KEY_GraveAccent, VK_OEM_3},
+        {KEY_LeftBracket, VK_OEM_4},
+        {KEY_RightBracket, VK_OEM_6},
+        {KEY_Apostrophe, VK_OEM_7},
     };
 
-    // -------------------------------------------------------
-    // Lazy-initialised lookup maps
-    // -------------------------------------------------------
-private:
-    static const std::unordered_map<int, int>& geodeToVkMap() {
-        static std::unordered_map<int, int> map = [] {
-            std::unordered_map<int, int> m;
-            for (const auto& p : kKeyTable)
-                m[static_cast<int>(p.geodeKey)] = p.vkCode;
-            return m;
-        }();
-        return map;
-    }
 
-    static const std::unordered_map<int, enumKeyCodes>& vkToGeodeMap() {
-        static std::unordered_map<int, enumKeyCodes> map = [] {
-            std::unordered_map<int, enumKeyCodes> m;
-            for (const auto& p : kKeyTable)
-                m[p.vkCode] = p.geodeKey;
-            return m;
-        }();
-        return map;
-    }
+    static std::optional<int> virtualKeyFromSetting(std::string const& setting) {
+        auto const keyBind = Mod::get()->getSettingValue<std::vector<Keybind>>(setting);
+        if (keyBind.empty()) return std::nullopt;
 
-public:
-    // -------------------------------------------------------
-    //  geodeKeyToVK
-    //
-    //  Converts a cocos2d::enumKeyCodes value (from a
-    //  geode::Keybind) to a Windows VK code.
-    // -------------------------------------------------------
-    static std::optional<int> geodeKeyToVK(enumKeyCodes key) {
-        const auto& map = geodeToVkMap();
-        auto it = map.find(static_cast<int>(key));
-        if (it != map.end())
-            return it->second;
+        // Retrieve the very last key within the bind array (for jokers using something like "CTRL+SHIFT+J" WHEN WE SAID IT'S NOT SUPPORTED)
+        enumKeyCodes const key = keyBind.back().key;
+
+        for (auto const [geodeKey, vkCode]: s_keyTable) {
+            if (geodeKey == key) return vkCode;
+        }
+
         return std::nullopt;
-    }
-
-    // -------------------------------------------------------
-    //  vkToGeodeKey
-    //
-    //  Converts a Windows VK code to a cocos2d::enumKeyCodes.
-    //  Returns std::nullopt if no mapping exists.
-    // -------------------------------------------------------
-    static std::optional<enumKeyCodes> vkToGeodeKey(int vkCode) {
-        const auto& map = vkToGeodeMap();
-        auto it = map.find(vkCode);
-        if (it != map.end())
-            return it->second;
-        return std::nullopt;
-    }
-
-    // -------------------------------------------------------
-    //  Convenience: get VK from the FIRST bind in a setting
-    //
-    //  Example:
-    //    auto vk = GeodeKeybindMapper::vkFromSetting("firstPlayerUpKey");
-    // -------------------------------------------------------
-    static std::optional<int> vkFromSetting(const auto binds) {
-        if (binds.empty()) return std::nullopt;
-        return geodeKeyToVK(binds[0].key);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,18 @@
-/**
- * Include the Geode headers.
- */
+// ReSharper disable CppHidingFunction
 #include <Geode/Geode.hpp>
 #include <Geode/loader/SettingV3.hpp>
 #include <Geode/loader/GameEvent.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/PauseLayer.hpp>
-#include "GeodeKeybindMapper.h"
 
-/**
- * Include the Windows header.
- */
 #include <windows.h>
 
-/**
- * Include our Geode to Windows mapper.
- */
 #include "GeodeKeybindMapper.h"
 
-/**
- * Brings cocos2d and all Geode namespaces to the current scope.
- */
 using namespace geode::prelude;
 
 
-/**
- * A global variable to store our hook handle.
- */
-HHOOK g_hook = nullptr;
+HHOOK gHook = nullptr;
 
 
 enum KeyAction {
@@ -37,18 +22,18 @@ enum KeyAction {
 };
 
 
-void handlePlayLayerButton(PlayLayer* pl, WPARAM wParam, KeyAction keyAction, bool isSecondPlayer = false) {
+void handlePlayLayerButton(PlayLayer* playLayer, WPARAM const wParam, KeyAction keyAction, bool isSecondPlayer = false) {
 	if (wParam == WM_KEYDOWN) {
-		queueInMainThread([pl, keyAction, isSecondPlayer] {
+		queueInMainThread([playLayer, keyAction, isSecondPlayer] {
 			switch (keyAction) {
-				case KeyAction::Left:
-					pl->handleButton(true, 0, isSecondPlayer);
+				case Left:
+					playLayer->handleButton(true, 0, isSecondPlayer);
 					break;
-				case KeyAction::Up:
-					pl->handleButton(true, 1, isSecondPlayer);
+				case Up:
+					playLayer->handleButton(true, 1, isSecondPlayer);
 					break;
-				case KeyAction::Right:
-					pl->handleButton(true, 2, isSecondPlayer);
+				case Right:
+					playLayer->handleButton(true, 2, isSecondPlayer);
 					break;
 				default:
 					break;
@@ -57,16 +42,16 @@ void handlePlayLayerButton(PlayLayer* pl, WPARAM wParam, KeyAction keyAction, bo
 	}
 
 	if (wParam == WM_KEYUP) {
-		queueInMainThread([pl, keyAction, isSecondPlayer] {
+		queueInMainThread([playLayer, keyAction, isSecondPlayer] {
 			switch (keyAction) {
-				case KeyAction::Left:
-					pl->handleButton(false, 0, isSecondPlayer);
+				case Left:
+					playLayer->handleButton(false, 0, isSecondPlayer);
 					break;
-				case KeyAction::Up:
-					pl->handleButton(false, 1, isSecondPlayer);
+				case Up:
+					playLayer->handleButton(false, 1, isSecondPlayer);
 					break;
-				case KeyAction::Right:
-					pl->handleButton(false, 2, isSecondPlayer);
+				case Right:
+					playLayer->handleButton(false, 2, isSecondPlayer);
 					break;
 				default:
 					break;
@@ -75,72 +60,62 @@ void handlePlayLayerButton(PlayLayer* pl, WPARAM wParam, KeyAction keyAction, bo
 	}
 }
 
+LRESULT CALLBACK KeyboardProc(int const code, WPARAM const wParam, LPARAM const lParam) {
+	if (!Mod::get()->getSettingValue<bool>("enabled")) {
+		return CallNextHookEx(gHook, code, wParam, lParam);
+	}
 
-LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam, LPARAM lParam) {
-    if (code == HC_ACTION) {
-        auto* kb = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
-        DWORD vk = kb->vkCode;
+	if (code == HC_ACTION) {
+		auto const keyBoard = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+		DWORD const virtualKey = keyBoard->vkCode;
 
-        auto player_layer = PlayLayer::get();
+		auto const playLayer = PlayLayer::get();
+		auto const scene = CCDirector::get()->getRunningScene();
 
-        auto first_player_up_key    = GeodeKeybindMapper::vkFromSetting(Mod::get()->getSettingValue<std::vector<geode::Keybind>>("firstPlayerUpKey"));
-        auto first_player_left_key  = GeodeKeybindMapper::vkFromSetting(Mod::get()->getSettingValue<std::vector<geode::Keybind>>("firstPlayerLeftKey"));
-        auto first_player_right_key = GeodeKeybindMapper::vkFromSetting(Mod::get()->getSettingValue<std::vector<geode::Keybind>>("firstPlayerRightKey"));
+		auto const firstPlayerUpKey    = GeodeKeybindMapper::virtualKeyFromSetting("firstPlayerUpKey");
+		auto const firstPlayerLeftKey  = GeodeKeybindMapper::virtualKeyFromSetting("firstPlayerLeftKey");
+		auto const firstPlayerRightKey = GeodeKeybindMapper::virtualKeyFromSetting("firstPlayerRightKey");
 
-        auto second_player_up_key    = GeodeKeybindMapper::vkFromSetting(Mod::get()->getSettingValue<std::vector<geode::Keybind>>("secondPlayerUpKey"));
-        auto second_player_left_key  = GeodeKeybindMapper::vkFromSetting(Mod::get()->getSettingValue<std::vector<geode::Keybind>>("secondPlayerLeftKey"));
-        auto second_player_right_key = GeodeKeybindMapper::vkFromSetting(Mod::get()->getSettingValue<std::vector<geode::Keybind>>("secondPlayerRightKey"));
+		auto const secondPlayerUpKey    = GeodeKeybindMapper::virtualKeyFromSetting("secondPlayerUpKey");
+		auto const secondPlayerLeftKey  = GeodeKeybindMapper::virtualKeyFromSetting("secondPlayerLeftKey");
+		auto const secondPlayerRightKey = GeodeKeybindMapper::virtualKeyFromSetting("secondPlayerRightKey");
 
-    	auto pause_game_key = GeodeKeybindMapper::vkFromSetting(Mod::get()->getSettingValue<std::vector<geode::Keybind>>("pauseGameKey"));
+		auto const pauseGameKey = GeodeKeybindMapper::virtualKeyFromSetting("pauseGameKey");
 
-        if (player_layer) {
-            if      (first_player_up_key    && vk == *first_player_up_key)    handlePlayLayerButton(player_layer, wParam, KeyAction::Up,    false);
-            else if (first_player_left_key  && vk == *first_player_left_key)  handlePlayLayerButton(player_layer, wParam, KeyAction::Left,  false);
-            else if (first_player_right_key && vk == *first_player_right_key) handlePlayLayerButton(player_layer, wParam, KeyAction::Right, false);
-            else if (second_player_up_key   && vk == *second_player_up_key)   handlePlayLayerButton(player_layer, wParam, KeyAction::Up,    true);
-            else if (second_player_left_key && vk == *second_player_left_key) handlePlayLayerButton(player_layer, wParam, KeyAction::Left,  true);
-            else if (second_player_right_key && vk == *second_player_right_key) handlePlayLayerButton(player_layer, wParam, KeyAction::Right, true);
-        }
+		if (playLayer) {
+			if      (firstPlayerUpKey    && virtualKey ==* firstPlayerUpKey)    handlePlayLayerButton(playLayer, wParam, Up,    false);
+			else if (firstPlayerLeftKey  && virtualKey ==* firstPlayerLeftKey)  handlePlayLayerButton(playLayer, wParam, Left,  false);
+			else if (firstPlayerRightKey && virtualKey ==* firstPlayerRightKey) handlePlayLayerButton(playLayer, wParam, Right, false);
+			else if (secondPlayerUpKey   && virtualKey ==* secondPlayerUpKey)   handlePlayLayerButton(playLayer, wParam, Up,    true);
+			else if (secondPlayerLeftKey && virtualKey ==* secondPlayerLeftKey) handlePlayLayerButton(playLayer, wParam, Left,  true);
+			else if (secondPlayerRightKey && virtualKey ==* secondPlayerRightKey) handlePlayLayerButton(playLayer, wParam, Right, true);
 
-        if (pause_game_key && vk == static_cast<DWORD>(*pause_game_key) && wParam == WM_KEYDOWN) {
-            queueInMainThread([] {
-                if (auto pl = PlayLayer::get()) {
-                    if (!pl->m_isPaused) {
-                        pl->pauseGame(false);
-                    } else {
-                        pl->resume();
-                        const auto scene = CCDirector::get()->getRunningScene();
-                        const auto pauseLayer = scene->getChildByType<PauseLayer>(0);
-                        pauseLayer->onResume(nullptr);
-                    }
-                }
-            });
-        }
-    }
+			if (pauseGameKey && virtualKey == static_cast<DWORD>(*pauseGameKey) && wParam == WM_KEYDOWN) {
+				queueInMainThread([scene, playLayer] {
+					if (!playLayer->m_isPaused) {
+						playLayer->pauseGame(false);
+					} else {
+						playLayer->resume();
+						auto const pauseLayer = scene->getChildByType<PauseLayer>(0);
+						pauseLayer->onResume(nullptr);
+					}
+				});
+			}
+		}
+	}
 
-    return CallNextHookEx(g_hook, code, wParam, lParam);
+	return CallNextHookEx(gHook, code, wParam, lParam);
 }
 
-class $modify(PlayLayer) {
-	void pauseGame(bool unfocused) {
-		if (unfocused) return;
-		PlayLayer::pauseGame(unfocused);
-	}
-
-	void publicPauseGame(bool unfocused) {
-		PlayLayer::pauseGame(unfocused);
-	}
-};
-
 $on_mod(Loaded) {
-	g_hook = SetWindowsHookEx(
+	gHook = SetWindowsHookEx(
 		WH_KEYBOARD_LL,
 		KeyboardProc,
 		nullptr,
 		0
 	);
 
-	if (!g_hook) {
+	if (!gHook) {
 		log::error("Hook failed: {}", GetLastError());
 	}
 }
