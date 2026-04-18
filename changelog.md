f19fafe0ceaa2f4302eb63ff557b1f1b599da0a4
Robin <robinrid6@gmail.com>
2026-04-18 17:49:03 +0200

    Removed queueInMainThread, added focus check and a timestamp to the queueButton function using getInputTimestamp()

--- changelog.md
+++ changelog.md
@@ -1,3 +1,11 @@
+# 2.0.2
+- Added a check for when the window is focussed or not.
+- Added a timestamp to our execution of queueButton.
+- We no longer use queueInMainThread.
+
+# 2.0.1
+- Added the "cheat" tag as our technique technically allows for pause buffering.
+
 # 2.0.0
 - Completely reworked the mod so it would align better with the Geode guidelines.
 - We now actually destroy the created hook after leaving the level to avoid left-over hooks.
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/modify/UILayer.hpp>
+#include <Geode/modify/CCEGLView.hpp>
 
 #include <windows.h>
 #include <unordered_set>
@@ -20,31 +21,35 @@ enum KeyAction {
     Right
 };
 
+bool isGDFocused() {
+    return GetActiveWindow() == WindowFromDC(wglGetCurrentDC());
+}
+
 void handleButton(WPARAM const wParam, KeyAction const keyAction, bool const isFirstPlayer = false) {
     bool const isDown = wParam == WM_KEYDOWN;
     PlayerButton button;
 
     switch (keyAction) {
-        case Left:  button = PlayerButton::Left; break;
-        case Up:    button = PlayerButton::Jump; break;
-        case Right: button = PlayerButton::Right; break;
-        default:    return;
+        case Left: button = PlayerButton::Left;
+            break;
+        case Up: button = PlayerButton::Jump;
+            break;
+        case Right: button = PlayerButton::Right;
+            break;
+        default: return;
     }
 
-    queueInMainThread([button, isDown, isFirstPlayer] {
-        auto const playLayer = PlayLayer::get();
-        if (!playLayer) return;
+    auto const playLayer = PlayLayer::get();
+    if (!playLayer) return;
 
-        playLayer->queueButton(static_cast<int>(button), isDown, !isFirstPlayer, false);
+    playLayer->queueButton(static_cast<int>(button), isDown, !isFirstPlayer, getInputTimestamp());
 
-        if (static_cast<int>(button) != 1) return;
+    if (button != PlayerButton::Jump) return;
+    auto const uiLayer = playLayer->getChildByType<UILayer>(0);
+    if (!uiLayer) return;
 
-        auto const uiLayer = playLayer->getChildByType<UILayer>(0);
-        if (!uiLayer) return;
-
-        if (isFirstPlayer) uiLayer->m_p1Jumping = isDown;
-        else uiLayer->m_p2Jumping = isDown;
-    });
+    if (isFirstPlayer) uiLayer->m_p1Jumping = isDown;
+    else uiLayer->m_p2Jumping = isDown;
 }
 
 void togglePause() {
@@ -52,23 +57,22 @@ void togglePause() {
     auto const playLayer = PlayLayer::get();
     if (!playLayer) return;
 
-    queueInMainThread([scene, playLayer] {
-        if (playLayer->m_isPaused) {
-            playLayer->resume();
-            if (auto const pauseLayer = scene->getChildByType<PauseLayer>(0)) pauseLayer->onResume(nullptr);
+    if (playLayer->m_isPaused) {
+        playLayer->resume();
+        if (auto const pauseLayer = scene->getChildByType<PauseLayer>(0)) pauseLayer->onResume(nullptr);
 
-            return;
-        }
+        return;
+    }
 
-        playLayer->pauseGame(false);
-    });
+    playLayer->pauseGame(false);
 }
 
 LRESULT CALLBACK keyboardProc(int const code, WPARAM const wParam, LPARAM const lParam) {
     if (!Mod::get()->getSettingValue<bool>("enabled")) return CallNextHookEx(s_hook, code, wParam, lParam);
+    if (isGDFocused()) return CallNextHookEx(s_hook, code, wParam, lParam);
     if (code != HC_ACTION) return CallNextHookEx(s_hook, code, wParam, lParam);
 
-    auto const keyBoard = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+    auto const keyBoard = reinterpret_cast<KBDLLHOOKSTRUCT *>(lParam);
     DWORD const virtualKey = keyBoard->vkCode;
 
     bool const isKeyDown = wParam == WM_KEYDOWN;
@@ -82,28 +86,29 @@ LRESULT CALLBACK keyboardProc(int const code, WPARAM const wParam, LPARAM const 
     if (isKeyUp) s_pressedKeys.erase(virtualKey);
 
     struct KeyBinding {
-        char const* setting;
+        char const *setting;
         KeyAction action;
         bool isFirstPlayer;
     };
 
     static KeyBinding const s_bindings[] = {
-        { "firstPlayerUpKey",     Up,    true  },
-        { "firstPlayerLeftKey",   Left,  true  },
-        { "firstPlayerRightKey",  Right, true  },
-        { "secondPlayerUpKey",    Up,    false },
-        { "secondPlayerLeftKey",  Left,  false },
-        { "secondPlayerRightKey", Right, false },
+        {"firstPlayerUpKey", Up, true},
+        {"firstPlayerLeftKey", Left, true},
+        {"firstPlayerRightKey", Right, true},
+        {"secondPlayerUpKey", Up, false},
+        {"secondPlayerLeftKey", Left, false},
+        {"secondPlayerRightKey", Right, false},
     };
 
-    for (const auto&[setting, action, isFirstPlayer] : s_bindings) {
+    for (const auto &[setting, action, isFirstPlayer]: s_bindings) {
         if (auto const key = GeodeKeybindMapper::virtualKeyFromSetting(setting); key && virtualKey == *key) {
             handleButton(wParam, action, isFirstPlayer);
             break;
         }
     }
 
-    if (auto const pauseGameKey = GeodeKeybindMapper::virtualKeyFromSetting("pauseGameKey"); pauseGameKey && virtualKey == *pauseGameKey && isKeyDown) {
+    if (auto const pauseGameKey = GeodeKeybindMapper::virtualKeyFromSetting("pauseGameKey");
+        pauseGameKey && virtualKey == *pauseGameKey && isKeyDown) {
         togglePause();
         return CallNextHookEx(s_hook, code, wParam, lParam);
     }
@@ -112,12 +117,12 @@ LRESULT CALLBACK keyboardProc(int const code, WPARAM const wParam, LPARAM const 
 }
 
 class $modify(PlayLayer) {
-    bool init(GJGameLevel* level, bool p1, bool p2) {
+    bool init(GJGameLevel *level, bool p1, bool p2) {
         if (!PlayLayer::init(level, p1, p2)) return false;
 
         if (!CCEGLView::get()->getIsFullscreen()) {
             s_hook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboardProc, nullptr, 0);
-            
+
             if (!s_hook) log::error("Hook failed: {}", GetLastError());
             else log::debug("Hook was created");
 
@@ -126,7 +131,6 @@ class $modify(PlayLayer) {
 
         return true;
     }
-
 
     void onQuit() {
         if (s_hook) {
